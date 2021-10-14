5d1c070539dcaac5db3337a779ca1898437380da
7000words <emekadrake707@gmail.com>
2021-10-14 22:06:28 +0100

    it works just need someone to test it

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+
 import time
-import pynput
+import random
 import webbrowser
 
+f = open("words.txt", "rt")
+word = f.read().split()
 
-#I don't know what to do or how to start this project.
+time.sleep(3)
+words = random.choice(word)
+
+edge_path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+
+webbrowser.register("microsoft edge",None, webbrowser.BackgroundBrowser(edge_path))
+
+web = webbrowser.get("microsoft edge").open(f"https://www.bing.com/search?q={words}")
